564c48201be152969898789563baeb7387212717
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-10-11 14:25:40 +0200

    creati prodotti cane e gatto

--- index.php
+++ index.php
@@ -37,6 +37,18 @@ class Product {
 $gatti = new Category('Gatti');
 $cani = new Category('Cani');
 
+$gattiProdotto = new Product(
+    'Prodotto per Gatti', 
+    9.99, 
+    'https://www.google.com/imgres?q=gatto&imgurl=https%3A%2F%2Fwww.robinsonpetshop.it%2Fnews%2Fcms2017%2Fwp-content%2Fuploads%2F2022%2F07%2Fgatto-vita.jpg&imgrefurl=https%3A%2F%2Fwww.robinsonpetshop.it%2Fnews%2Fgatto%2Fle-fasi-di-vita-del-gatto-domestico%2F&docid=OW_oKL0k_c2QrM&tbnid=uvlx_6LZTFAkjM&vet=12ahUKEwjv5bvRp4aJAxXogf0HHZeiLC0QM3oECFAQAA..i&w=800&h=400&hcb=2&ved=2ahUKEwjv5bvRp4aJAxXogf0HHZeiLC0QM3oECFAQAA',
+    $gatti);
+
+$caniProdotto = new Product(
+    'Prodotto per Cani', 
+    9.59, 
+    'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.purinashop.it%2Fblog%2Fcuriosita-cani&psig=AOvVaw0gAtqbX0bOn9hlolYylmeO&ust=1728735853285000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJiio52phokDFQAAAAAdAAAAABAE',
+    $cani);
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
