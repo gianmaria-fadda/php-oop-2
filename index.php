45b21b685e14d6303ca631e5350b066e47cefaa7
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-10-11 14:11:09 +0200

    Create classi Category e Product

--- index.php
+++ index.php
@@ -1,5 +1,33 @@
 <?php
 
+class Category {
+
+    public $name;
+    public $icon;
+
+}
+
+class Product {
+
+    public $title;
+    public $price;
+    public $img;
+    protected $category;
+
+    function __construct(Category $category) {
+        $this->setCategory($category);
+    }
+
+    public function getCategory() {
+        return $this->category;
+    }
+
+    public function setCategory(Category $category) {
+        $this->category = $category;
+    }
+
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
